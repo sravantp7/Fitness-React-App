e406d3a078e1fd478699bc4500dc0e85784a5825
Sravan T P <sravantp03@gmail.com>
2023-01-24 11:19:06 +0530

    added hamburger menu option for smaller screens

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 import Logo from '@/assets/Logo.png';
 import Link from "./Link";
 import { SelectedPage } from '@/shared/types';
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -11,6 +12,9 @@ type Props = {
 
 const Navbar = ({selectedPage, setSelectedPage}: Props) => {
   const flexBetween = 'flex items-center justify-between';
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+
   return (
     <nav>
         <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -18,8 +22,9 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                 <div className={`${flexBetween} w-full gap-16`}> 
                     {/*Left Side  */}
                     <img src={Logo} alt="logo" />
+
                     {/* Right Side */}
-                    <div className={`${flexBetween} w-full`}>
+                    { isAboveMediumScreens ? (<div className={`${flexBetween} w-full`}>
                         {/* Links */}
                         <div className={`${flexBetween} gap-8 text-sm`}>
                             <Link page='Home' selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
@@ -33,7 +38,11 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                             <p>Sign In</p>
                             <button>Become a Member</button>
                         </div>
-                    </div>
+                    </div> ) : (
+                        <button className='rounded-full bg-secondary-500 p-2' onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                           {!isMenuToggled ? <Bars3Icon className="h-6 w-6 text-white"/> : <XMarkIcon className="h-6 w-6 text-white"/> }
+                        </button>
+                    )}
                 </div>
             </div>
         </div>
