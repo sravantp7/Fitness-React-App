20d9640b1ea27cfabda5dc2d2f3090cc5aeb094d
Sravan T P <sravantp03@gmail.com>
2023-01-25 14:29:16 +0530

    added animations to benefit section

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -8,29 +8,38 @@ type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 }
 
+const container = {
+  hidden: {},
+  visible:{
+    transition: { staggerChildren: 0.2}
+  }
+}
+
 const Benefits = ({setSelectedPage}: Props) => {
   return (
     <section id='benefits' className='mx-auto min-h-full py-20 w-5/6'>
       <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
 
         {/* Header */}
-        <div className='md:my-5 md:w-3/5'>
+        <motion.div className='md:my-5 md:w-3/5'
+          initial='hidden' whileInView='visible' viewport={{once: true, amount: 0.5}} transition={{duration: 0.5}} variants={{hidden: {opacity: 0, x:-50}, visible: {opacity:1, x:0}}} >
           <HText>more than just a gym.</HText>
           <p className='my-5 text-sm'>
             We provide world class fitness equipment, trainers and classes to
             get you to your ultimate fitness goals with ease. We provide true
             care into each and every member.
           </p>
-        </div>
+        </motion.div>
 
         {/* Benefits */}
-        <div className='md:flex items-center justify-between gap-8 mt-5'>
+        <motion.div className='md:flex items-center justify-between gap-8 mt-5'
+          initial='hidden' whileInView='visible' viewport={{once: true, amount: 0.5}} variants={container} >
             {
               benefits.map((benefit) => (
                 <BenefitsSection benefit={benefit} setSelectedPage={setSelectedPage} key={benefit.title}/>
               ))
             }
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   );
--- src/components/Benefits/BenefitsSection.tsx
+++ src/components/Benefits/BenefitsSection.tsx
@@ -1,4 +1,5 @@
 import { BenefitType, SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
@@ -6,9 +7,15 @@ type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 }
 
+const childVariant = {
+    hidden: { opacity: 0, scale: 0.9},
+    visible: {opacity: 1, scale: 1}
+}
+
 const BenefitsSection = ({benefit, setSelectedPage}: Props) => {
   return (
-    <div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center">
+    <motion.div className="mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center"
+        variants={childVariant} >
         <div className="mb-4 flex justify-center">
             <div className="rounded-full border-2 border-gray-100 bg-primary-100 p-4">
                 {benefit.icon}
@@ -17,7 +24,7 @@ const BenefitsSection = ({benefit, setSelectedPage}: Props) => {
         <h4 className="font-bold">{benefit.title}</h4>
         <p className="my-3">{benefit.description}</p>
         <AnchorLink href={`${SelectedPage.ContactUs}`} className='text-sm font-bold text-primary-500 underline hover:text-secondary-500 cursor-pointer' onClick={() => setSelectedPage(SelectedPage.ContactUs)} ><p>Learn More</p></AnchorLink>
-    </div>
+    </motion.div>
   );
 }
 
